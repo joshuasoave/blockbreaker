33b698fd21988674460cffa4b9ea83172121bec3
Joshua Soave <joshua.soave@gmail.comgit config --global user.name JoshuaSoave>
2020-09-14 19:36:30 -0400

    uploading the c# scripts for my unity block breaker game

---
+++ Scripts/Block.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Block : MonoBehaviour
+{
+    //config params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+
+    //cached ref
+    Level level;
+
+    //state vars
+    [SerializeField] int timesHit; //TODO only serialized for debug purposes
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    //private void Update()
+    //{
+    //    level.CountBreakableBlocks();
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if(hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block Sprite is missing from array on " + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockDestroySFX();
+        TriggerSparklesVFX();
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
---
+++ Scripts/Paddle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //configuration paramaters 
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float xMin = 0f;
+    [SerializeField] float xMax = 15f;
+
+    //cached ref
+    Ball theBall;
+    GameSession theGameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theBall = FindObjectOfType<Ball>();
+        theGameSession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), xMin, xMax);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if(theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        } else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
